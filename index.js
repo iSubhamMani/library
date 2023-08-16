94a93cf906c59b44bb960944767ba2d1177d9cd6
Subham Mani <manisubham09@gmail.com>
2023-08-16 13:25:08 +0530

    add Book obj on form submit

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const myLibrary = [];
-
 const addBookBtn = document.querySelector('.add-book-btn');
 const modal = document.querySelector('[data-modal]');
 const cancelModal = document.getElementById('cancel-modal');
+const submitForm = document.getElementById('submit-modal');
+const form = document.querySelector('.form');
+
+const bookTitle = document.getElementById('book-title');
+const bookAuthor = document.getElementById('book-author');
+const bookPages = document.getElementById('book-pages');
+const bookRead = document.getElementById('book-read');
+
+const myLibrary = [];
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -11,8 +18,25 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
-function addBookToLibrary(){
+function resetForm(){
+    modal.close();
+    form.reset();
+    bookRead.setAttribute('data-value', 'false');
+}
 
+function addBookToLibrary(){
+    // All fields are filled
+    if(bookTitle.value !== '' && bookAuthor.value !== '' &&
+    bookPages.value !== ''){
+        // Push a new book object to myLibrary arr
+        myLibrary.push(new Book(bookTitle.value, bookAuthor.value,
+            bookPages.value, bookRead.getAttribute('data-value')));
+
+        // Reset Form
+        resetForm();
+        console.log(myLibrary);
+    }
+    else return;
 }
 
 addBookBtn.addEventListener('click', () => {
@@ -21,4 +45,19 @@ addBookBtn.addEventListener('click', () => {
 
 cancelModal.addEventListener('click', () => {
     modal.close();
+})
+
+bookRead.addEventListener('click', () => {
+    if(bookRead.getAttribute('data-value') === 'false'){
+        bookRead.setAttribute('data-value', 'true');
+    }
+    else if(bookRead.getAttribute('data-value') === 'true'){
+        bookRead.setAttribute('data-value', 'false');
+    }
+})
+
+submitForm.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    addBookToLibrary();
 })
